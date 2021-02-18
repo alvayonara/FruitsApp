45b2683d0d468745712c71d903a891037c8c3671
Alva Yonara Puramandya <alvayonara@outlook.com>
2021-02-18 20:12:34 +0700

    Init project and create Onboarding screen using PageTabView

---
+++ FruitsApp/App/FruitsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsAppApp.swift
+//  FruitsApp
+//
+//  Created by Alva Yonara Puramandya on 18/02/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
---
+++ FruitsApp/App/OnboardingView.swift
@@ -0,0 +1,30 @@
+//
+//  OnboardingView.swift
+//  FruitsApp
+//
+//  Created by Alva Yonara Puramandya on 18/02/21.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        TabView {
+            ForEach(0..<5) { item in
+                FruitCardView()
+            } //: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+
+// MARK: - PREVIEW
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
